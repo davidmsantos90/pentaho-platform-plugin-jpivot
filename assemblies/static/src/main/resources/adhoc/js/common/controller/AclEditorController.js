e64fff9453927f065a2d1bcad7b5c680d3d543ec
David Alexandre Marques dos Santos <dsantos@pentaho.com>
2019-11-15 16:40:18 +0000

    [CLEANUP] code cleanup

--- assemblies/static/src/main/resources/adhoc/js/common/controller/AclEditorController.js
+++ assemblies/static/src/main/resources/adhoc/js/common/controller/AclEditorController.js
@@ -1,6 +1,6 @@
 /*
-* Copyright 2002 - 2017 Hitachi Vantara.  All rights reserved.
-* 
+* Copyright 2002 - 2019 Hitachi Vantara.  All rights reserved.
+*
 * This software was developed by Hitachi Vantara and is provided under the terms
 * of the Mozilla Public License, Version 1.1, or any later version. You may not use
 * this file except in compliance with the license. If you need a copy of the license,
@@ -11,192 +11,160 @@
 * the license for the specific language governing your rights and limitations.
 */
 
-AclEditorController = function( dialog )
-{
+AclEditorController = function(dialog) {
   this.dialog = dialog;
-  this.dialog.setTitle( AclEditorController.DIALOG_TITLE );
+
+  this.dialog.setTitle(AclEditorController.DIALOG_TITLE);
   this.aclCtrl = dialog.getAclEditorCtrl();
-  this.dialog.enableOkBtnEl( false );
+  this.dialog.enableOkBtnEl(false);
+
   var localThis = this;
-  
-  dialog.setOnSaveHandler( 
-    function( event )
-    {
-      var value = localThis.aclCtrl.getValue();
-      localThis.saveAcl( value );
-    }
-  );
-  
-  dialog.setOnCancelHandler( 
-    function( event )
-    {
-      localThis.dialog.hide();
-    }
-  );
-  
+
+  dialog.setOnSaveHandler(function(event) {
+    var value = localThis.aclCtrl.getValue();
+    localThis.saveAcl( value );
+  });
+
+  dialog.setOnCancelHandler(function(event) {
+    localThis.dialog.hide();
+  });
+
   // as soon as any checkbox is modified, enable the Ok button
-  this.aclCtrl.setOnItemCheckHandler(
-    function( event )
-    {
-      localThis.dialog.enableOkBtnEl( localThis.canEnableOkBtn() );
-    }
-  );
+  this.aclCtrl.setOnItemCheckHandler(function(event) {
+    localThis.dialog.enableOkBtnEl( localThis.canEnableOkBtn() );
+  });
 };
 
-/*private static*/AclEditorController.PENTAHO_HTTP_WEBSERVICE_URL = "../ServiceAction";
-/*private static*/AclEditorController.SOLUTION_REPOSITORY_WEBSERVICE_URL = "../SolutionRepositoryService";
+/*private static*/
+AclEditorController.PENTAHO_HTTP_WEBSERVICE_URL = "../ServiceAction";
+
+/*private static*/
+AclEditorController.SOLUTION_REPOSITORY_WEBSERVICE_URL = "../SolutionRepositoryService";
+
 // TODO sbarkdull, need to localize text
-/*private static*/AclEditorController.FILE_LABEL = Messages.getString( "PATH_LABEL" );
-/*private static*/AclEditorController.DIALOG_TITLE = Messages.getString( "SHARE_PERMISSIONS" );
+/*private static*/
+AclEditorController.FILE_LABEL = Messages.getString("PATH_LABEL");
+
+/*private static*/
+AclEditorController.DIALOG_TITLE = Messages.getString("SHARE_PERMISSIONS");
 
 /**
- * 
+ *
  */
-AclEditorController.prototype.loadPage = function( solution, path, filename )
-{
+AclEditorController.prototype.loadPage = function(solution, path, filename) {
   this.solution = solution;
   this.path = path;
   this.filename = filename;
+
+  var title = AclEditorController.FILE_LABEL + "/" + solution + ( path != "" ? "/" : "" ) + path + "/" + filename;
+  this.aclCtrl.setTitle(title);
+
   var localThis = this;
-  this.aclCtrl.setTitle( AclEditorController.FILE_LABEL 
-    + "/" + solution 
-    + ( path != "" ? "/" : "" ) + path 
-    + "/" + filename );
-  this.loadUsers( 
-    function( xmlDoc )
-    { 
-      if ( undefined != xmlDoc )
-      {
+
+  this.loadUsers(function(xmlDoc) {
+    if (undefined != xmlDoc) {
+      var errorMsg = XmlUtil.getErrorMsg(xmlDoc);
+      if (!errorMsg) {
+        localThis.aclCtrl.setUsers( xmlDoc );
+      } else {
+        alert( errorMsg );  // TODO sbarkdull, better msg UI?
+      }
+    }
+
+
+    localThis.loadRoles(function(xmlDoc) {
+      if (undefined != xmlDoc) {
         var errorMsg = XmlUtil.getErrorMsg( xmlDoc );
-        if ( !errorMsg )
-        {
-          localThis.aclCtrl.setUsers( xmlDoc );
-        }
-        else
-        {
+        if (!errorMsg) {
+          localThis.aclCtrl.setRoles( xmlDoc );
+        } else {
           alert( errorMsg );  // TODO sbarkdull, better msg UI?
         }
       }
-      // start loadRoles --------------------------
-      localThis.loadRoles( 
-        function( xmlDoc )
-        { 
-          if ( undefined != xmlDoc )
-          {
-            var errorMsg = XmlUtil.getErrorMsg( xmlDoc );
-            if ( !errorMsg )
-            {
-              localThis.aclCtrl.setRoles( xmlDoc );
-            }
-            else
-            {
-              alert( errorMsg );  // TODO sbarkdull, better msg UI?
-            }
+
+      localThis.loadAcl(function(xmlDoc) {
+        if (undefined != xmlDoc) {
+          var errorMsg = XmlUtil.getErrorMsg( xmlDoc );
+          if (!errorMsg) {
+            localThis.aclCtrl.setAcl( xmlDoc );
+          } else {
+            alert( errorMsg );  // TODO sbarkdull, better msg UI?
           }
-          
-          // start loadAcl --------------------------
-          localThis.loadAcl(
-            function( xmlDoc )
-            { 
-              if ( undefined != xmlDoc )
-              {
-                var errorMsg = XmlUtil.getErrorMsg( xmlDoc );
-                if ( !errorMsg )
-                {
-                  localThis.aclCtrl.setAcl( xmlDoc );
-                }
-                else
-                {
-                  alert( errorMsg );  // TODO sbarkdull, better msg UI?
-                }
-              }
-            }
-          );
-          // end loadAcl --------------------------
         }
-      );
-      // end loadRoles --------------------------
-    }
-  );
+      }); // end loadAcl --------------------------
+
+    }); // end loadRoles --------------------------
+  });
 };
 
-AclEditorController.prototype.saveAcl = function( strXml )
-{
+AclEditorController.prototype.saveAcl = function(strXml) {
   // TODO sbarkdull, show "working" and "hour glass", see WAQR code
-  this.dialog.enableOkBtnEl( false );
-  this.dialog.enableCancelBtnEl( false );
+  this.dialog.enableOkBtnEl(false);
+  this.dialog.enableCancelBtnEl(false);
+
+  var url = AclEditorController.SOLUTION_REPOSITORY_WEBSERVICE_URL;
+  var parameters = {
+    solution: this.solution,
+    path: this.path,
+    filename: this.filename,
+    aclXml: strXml
+  };
+
   var localThis = this;
-  WebServiceProxy.post( AclEditorController.SOLUTION_REPOSITORY_WEBSERVICE_URL, "setAcl",
-    {
-      solution: this.solution,
-      path: this.path,
-      filename: this.filename,
-      aclXml: strXml
-    },
-    function( xmlDoc )
-    {
-      localThis.dialog.enableOkBtnEl( true );
-      localThis.dialog.enableCancelBtnEl( true );
-      if ( undefined != xmlDoc )
-      {
-        var errorMsg = XmlUtil.getErrorMsg( xmlDoc );
-        if ( errorMsg )
-        {
-          alert( errorMsg );  // TODO sbarkdull, better msg UI?
-        }
-        else
-        {
+
+  WebServiceProxy.post(url, "setAcl", parameters, function(xmlDoc) {
+      localThis.dialog.enableOkBtnEl(true);
+      localThis.dialog.enableCancelBtnEl(true);
+
+      if (undefined != xmlDoc) {
+        var errorMsg = XmlUtil.getErrorMsg(xmlDoc);
+        if (errorMsg) {
+          alert(errorMsg);  // TODO sbarkdull, better msg UI?
+        } else {
           localThis.dialog.hide();
-// NO NEED TO SHOW DIALOG SAYING SAVE WORKED, USEFUL FOR DEBUGGING
-//  				var statusMsg = XmlUtil.getStatusMsg( xmlDoc );
-//  				if ( statusMsg )
-//  				{
-//  					alert( statusMsg );
-//  				}
+
+          // NO NEED TO SHOW DIALOG SAYING SAVE WORKED, USEFUL FOR DEBUGGING
+          // var statusMsg = XmlUtil.getStatusMsg(xmlDoc);
+          // if (statusMsg) {
+          //     alert( statusMsg );
+          // }
         }
       }
     }
   );
 };
 
-AclEditorController.prototype.loadAcl = function( onLoadHandler )
-{
-  var localThis = this;
-  WebServiceProxy.post( AclEditorController.SOLUTION_REPOSITORY_WEBSERVICE_URL, "getAcl",
-    {
-      solution: localThis.solution,
-      path: localThis.path,
-      filename: localThis.filename
-    },
-    onLoadHandler
-  );
+AclEditorController.prototype.loadAcl = function(onLoadHandler) {
+  var url = AclEditorController.SOLUTION_REPOSITORY_WEBSERVICE_URL;
+  var parameters = {
+    solution: this.solution,
+    path: this.path,
+    filename: this.filename
+  };
+
+  WebServiceProxy.post(url, "getAcl", parameters, onLoadHandler);
 };
 
-AclEditorController.prototype.loadUsers = function( onLoadHandler )
-{
-  var localThis = this;
-  WebServiceProxy.post( AclEditorController.PENTAHO_HTTP_WEBSERVICE_URL, undefined,
-    {
-      action:"securitydetails",
-      details:"users"
-    },
-    onLoadHandler
-  );
+AclEditorController.prototype.loadUsers = function(onLoadHandler) {
+  var url = AclEditorController.PENTAHO_HTTP_WEBSERVICE_URL;
+  var parameters = {
+    action:"securitydetails",
+    details:"users"
+  };
+
+  WebServiceProxy.post(url, undefined, parameters, onLoadHandler);
 };
 
-AclEditorController.prototype.loadRoles = function( onLoadHandler )
-{
-  var localThis = this;
-  WebServiceProxy.post( AclEditorController.PENTAHO_HTTP_WEBSERVICE_URL, undefined,
-    {
-      action:"securitydetails",
-      details:"roles"
-    },
-    onLoadHandler
-  );
+AclEditorController.prototype.loadRoles = function(onLoadHandler) {
+  var url = AclEditorController.PENTAHO_HTTP_WEBSERVICE_URL;
+  var parameters = {
+    action:"securitydetails",
+    details:"roles"
+  };
+
+  WebServiceProxy.post(url, undefined, parameters, onLoadHandler);
 };
 
-AclEditorController.prototype.canEnableOkBtn = function()
-{
+AclEditorController.prototype.canEnableOkBtn = function() {
   return true;
 };
--- assemblies/static/src/main/resources/adhoc/js/common/server/WebServiceProxy.js
+++ assemblies/static/src/main/resources/adhoc/js/common/server/WebServiceProxy.js
@@ -1,6 +1,6 @@
 /*
 * Copyright 2002 - 2017 Hitachi Vantara.  All rights reserved.
-* 
+*
 * This software was developed by Hitachi Vantara and is provided under the terms
 * of the Mozilla Public License, Version 1.1, or any later version. You may not use
 * this file except in compliance with the license. If you need a copy of the license,
@@ -11,7 +11,7 @@
 * the license for the specific language governing your rights and limitations.
 */
 
- document.write("<script type='text/javascript' src='webcontext.js'></script>"); 
+ document.write("<script type='text/javascript' src='webcontext.js'></script>");
 /**
  * This class provides a mechanism for calling a Hitachi Vantara WebService using AJAX techniques.
  * This is a stateless, static class, you should never call the ctor
@@ -26,61 +26,57 @@ WebServiceProxy = function()
 
 /**
  * Make a call to the WebService
- * 
- * @param String url base URL to the web service (eg "AdhocWebService")
- * @param component String referring to the string selector in the adhoc web service
- * that determines which "service" will be called.
- * @parm params Object containing properties, these properties and their values
- * will be placed in the query string of the web service call
- * @param funk callback function to be called when asynchronous call returns. If this
- * value is null or undefined, the call will be made synchronously. when the callback is
- * called, if the user session has expire, funk will be passed a parameter
- * set to undefined, otherwise it will be passed the XML document (XDocument, see dom.js)
- * return be the server.
- * @param mimeType String specifying the mime type, usually text/xml, text/text, tea/bag
+ * @static
+ *
+ * @param {String} url - base URL to the web service (eg "AdhocWebService")
+ * @param {String} component - String referring to the string selector in the adhoc web service
+ *                             that determines which "service" will be called.
+ * @param {Object} params - Object containing properties, these properties and their values
+ *                          will be placed in the query string of the web service call
+ * @param {Function} callback - function to be called when asynchronous call returns. If this
+ *                              value is null or undefined, the call will be made synchronously. when the callback is
+ *                              called, if the user session has expire, callback will be passed a parameter
+ *                              set to undefined, otherwise it will be passed the XML document (XDocument, see dom.js)
+ *                              return be the server.
+ * @param {String} [mimeType] - String specifying the mime type, usually text/xml, text/text, tea/bag
+ *
  * @return returns null if called asynchronously. When called synchronously, if the user
- * session has expired, it returns undefined, else returns the xml document
- * (XDocument, see dom.js) which has been initialized by the xml
- * string returned by the server.
+ *         session has expired, it returns undefined, else returns the xml document
+ *         (XDocument, see dom.js) which has been initialized by the xml
+ *         string returned by the server.
  */
-/*public static*/WebServiceProxy.post = function( url, component, params, funk, mimeType ) {
-	var query = "";
-	
-	if ( !params )
-	{
-		params = new Object();
-	}
-  params[ "ajax" ] = "true";
-  if( component ) {
-	  params[ "component" ] = component;
+WebServiceProxy.post = function(url, component, params, callback, mimeType) {
+  var query = "";
+
+  if (!params) {
+    params = new Object();
   }
 
-  for ( paramName in params )
-  {
-    query += encodeURIComponent( paramName ) + "=" + encodeURIComponent( params[paramName] ) + "&";
-  }
-  if ( query.charAt( query.length-1 ) == "&" )
-  {
-  	query = query.substr( 0, query.length-1 );
-  }
-  var xmlDoc = null;
-  if ( funk )
-  {
-  	pentahoPost( url, query, 
-			function( xmlStr )
-			{
-				WebServiceProxy.handleResponse( xmlStr, funk ); // cool, funk is in scope
-			},
-			mimeType );
-  }
-  else
-  {
-  	var xmlStr = pentahoPost( url, query, undefined, mimeType );
-		xmlDoc = WebServiceProxy.handleResponse( xmlStr, funk ); // funk should be undefined
+  params["ajax"] = "true";
+  if (component) {
+    params["component"] = component;
   }
 
-  return xmlDoc;
-}
+  for (var paramName in params) {
+    query += encodeURIComponent(paramName) + "=" + encodeURIComponent(params[paramName]) + "&";
+  }
+
+  if (query.charAt(query.length-1) == "&") {
+    query = query.substr( 0, query.length-1 );
+  }
+
+  if (callback) {
+    pentahoPost(url, query, function(response) {
+      WebServiceProxy.handleResponse(response, callback);
+    }, mimeType);
+
+    return null;
+  }
+
+  var response = pentahoPost(url, query, undefined, mimeType);
+
+  return WebServiceProxy.handleResponse(response, callback); // callback should be undefined
+};
 
 /**
  * @param xmlStr String containing the contents of the xml document
@@ -92,8 +88,8 @@ WebServiceProxy = function()
  * 	String the name of a javascript function object
  * funk will be called and passed an xml document (XDocument). The document will contain
  * the contents of the parameter xmlStr.
- * 
- * @return if the user is authenticated, and running synchronously, return 
+ *
+ * @return if the user is authenticated, and running synchronously, return
  * the xml document received from the server, otherwise return null.
  * If the user is not authenticated, return undefined.
  */
@@ -136,7 +132,7 @@ WebServiceProxy = function()
 /**
  * Call the function or method identified by the parameter funk, and pass the
  * function the parameter xmlDoc.
- * 
+ *
  * @param funk type can be one of:
  * 	Object with properties obj and method: obj must be an javascript Object, and method is the
  * 	method to be called on obj (obj.method()).
@@ -144,9 +140,9 @@ WebServiceProxy = function()
  * 	String the name of a javascript function object
  * funk will be called and passed the parameter xmlDoc.
  * @param xmlDoc XDocument an xml document containing the xml returned from the server.
- * 
+ *
  * @return whatever value is returned by funk
- * 
+ *
  * @throws Error when funk is an unrecognized object
  */
 /*private static*/WebServiceProxy.executeFunk = function( funk, xmlDoc )
